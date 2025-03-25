559b3007446ab2f925408cb3364e00fde861ac1f
Nariman <aliyevnariman98@gmail.com>
2025-03-25 17:02:55 +0100

    the beginning of the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-[url('/bg-image.png')] h-screen w-full">
       <div className="flex flex-col items-center justify-center h-screen">
-        <div className="relative w-[289px] h-[222px] lg:w-[489px] lg:h-[311px] z-10">
+        <div className="relative w-[171px] h-[130px] lg:w-[303px] lg:h-[230px] z-10">
           <Image
             fill
             src={"/coming soon.png"}
@@ -12,7 +12,7 @@ export default function Home() {
             alt="coming soon"
           />
         </div>
-        <h3 className="text-white text-[33px] lg:text-[51px] font-[300]">
+        <h3 className="text-white text-[20px] lg:text-[34px] font-[300]">
           Soon. It&rsquo;s becoming
         </h3>
       </div>
