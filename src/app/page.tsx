db7a3da83bec11408b167a084c3f1190ee695e6a
Nariman <aliyevnariman98@gmail.com>
2025-03-26 19:51:48 +0100

    the beginning of the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="sm:bg-[url('/bg-image.png')] bg-[url('/rotatedimg.png')] bg-cover min-h-screen w-full">
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center min-h-screen">
         <div className="relative w-[171px] h-[130px] lg:w-[303px] lg:h-[230px] z-10">
           <Image
             fill
             src={"/coming soon.png"}
             className="object-contain"
             alt="coming soon"
+            priority
           />
         </div>
         <h3 className="text-white text-[20px] lg:text-[34px] font-[300]">
