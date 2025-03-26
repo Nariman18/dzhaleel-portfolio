abafed78b5321c831b9cdb28ad5a208ba2e100bc
Nariman <aliyevnariman98@gmail.com>
2025-03-26 19:25:51 +0100

    the beginning of the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-[url('/bg-image.png')] h-screen w-full">
+    <div className="relative">
+      <div className="absolute inset-0 sm:bg-[url('/bg-image.png')] bg-[url('/rotatedimg.png')]"></div>
       <div className="flex flex-col items-center justify-center h-screen">
         <div className="relative w-[171px] h-[130px] lg:w-[303px] lg:h-[230px] z-10">
           <Image
@@ -12,7 +13,7 @@ export default function Home() {
             alt="coming soon"
           />
         </div>
-        <h3 className="text-white text-[20px] lg:text-[34px] font-[300]">
+        <h3 className="text-white text-[20px] lg:text-[34px] font-[300] z-10">
           Soon. It&rsquo;s becoming
         </h3>
       </div>
